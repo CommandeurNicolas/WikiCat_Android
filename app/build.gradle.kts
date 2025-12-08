416bc7019635e2ffebbf8e2179cb051653d48f3e
Nicolas Commandeur <comcomnico@gmail.com>
2025-12-08 20:02:17 +0100

    Moving libraries declarations to library catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-    testImplementation("app.cash.turbine:turbine:1.2.1")
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
@@ -94,7 +94,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
 
     // * Hilt x Compose navigation
-    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+    implementation(libs.androidx.hilt.navigation.compose)
 }
 
 // ! Needed to align to Kotlin version
